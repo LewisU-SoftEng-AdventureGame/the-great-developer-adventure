42172c20ef171150130cec3d8771d5af1b884b86
Alison Major <alison.major@gmail.com>
2021-01-31 08:47:31 -0500

    Add failing test
    This is to test our pipeline in branches.

--- tests/test_validate_email.py
+++ tests/test_validate_email.py
@@ -4,7 +4,8 @@ import pytest
 @pytest.mark.parametrize('email, is_okay', [
     ('john.doe@somecompany.com', True),
     ('asdfjkl;lkj', False),
-    ('', False)
+    ('', False),
+    ('test to force it to fail', True)
 ])
 def test_validate_email(email, is_okay):
     assert validate_email(email) == is_okay
